b8401295014a87838880dc1b853c6911a63be30f
jacks1718 <javak.maravi@gmail.com>
2024-07-03 14:01:15 +0200

    first commit

---
+++ src/main/java/com/PyroMap/PyroMapApplication.java
@@ -0,0 +1,13 @@
+package com.PyroMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyroMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyroMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PyroMap/PyroMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PyroMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyroMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
